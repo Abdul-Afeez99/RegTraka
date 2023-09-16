4c8bacaefafa478f9c372444d3fb344f8ec4a585
AbdulAfeez Adeyemo <114268023+Abdul-Afeez99@users.noreply.github.com>
2023-09-16 18:32:35 +0100

    Update serializers.py

--- Users/serializers.py
+++ Users/serializers.py
@@ -5,26 +5,6 @@ from rest_framework.authentication import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import update_last_login
 
-def get_all_schools():
-        available_schools = list(CustomUser.administrator.get_queryset().values('name'))
-        list_of_schools = []
-        for school in available_schools:
-            list_of_schools.append(school['name'])
-        return list_of_schools
-    
-def get_all_courses(school, year):
-    available_courses = list(Courses.objects.filter(school=school, year=year).values('title'))
-    list_of_courses = []
-    for course in available_courses:
-        list_of_courses.append(course['title'])
-    return list_of_courses
-
-def get_available_year(school):
-    available_year = list(Year.objects.filter(school=school).values('name'))
-    list_of_years = []
-    for school in available_year:
-        list_of_years.append(school['name'])
-    return list_of_years
 
 sex = [
         ("MALE", "Male"),
@@ -139,35 +119,7 @@ class LoginSerializer(serializers.ModelSerializer):
 # Student registration
 class StudentRegistrationSerializer(serializers.ModelSerializer):
     gender = serializers.ChoiceField(choices=sex)
-    school = serializers.ChoiceField(choices=[])
-    year = serializers.ChoiceField(choices=[])
-    courses = serializers.MultipleChoiceField(choices=[])
-    
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['school'].choices = get_all_schools()
-    
-    def validate_school(self, value):
-        self.fields['year'].choices = get_available_year(value)
-        return value
-    
-    def validate_year(self, value):
-        school_id = self.initial_data.get('school')  # Get the school name from the initial data
-        print(school_id)
-        
-        try:
-            school = Administrator.objects.get(id=school_id)
-        except Administrator.DoesNotExist:
-            raise serializers.ValidationError("Invalid school id")  # Handle the case where the school doesn't exist
-        
-        year = Year.objects.get(school=school)
-        courses = get_all_courses(school=school, year=year)
-        self.fields['courses'].choices = courses
-        return value
-
+  
     class Meta:
         model = Student
-        fields = [
-            'name', 'school', 'matric_no', 'gender', 'year',
-            'image', 'courses'
-        ]
+        fields = '__all__'
