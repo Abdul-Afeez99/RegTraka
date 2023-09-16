8f495f4ad422ddc185699f3a28dc64bc7b0f7f20
AbdulAfeez Adeyemo <114268023+Abdul-Afeez99@users.noreply.github.com>
2023-09-16 18:36:11 +0100

    Update serializers.py

--- administrator/serializers.py
+++ administrator/serializers.py
@@ -1,50 +1,23 @@
 from Users.models import Courses, Instructor, Administrator, Student, Year
 from rest_framework import serializers
 
-# Function to retrieve the list of instructors for the current user
-def get_available_instructors(user):
-    school_object = Administrator.objects.get(user=user)
-    available_instructors = list(Instructor.objects.filter(school=school_object).values_list('name'))
-    return available_instructors
-
-# function to retrieve available classrooms in the school
-def get_available_year(user):
-    school_object = Administrator.objects.get(user=user)
-    available_year = list(Year.objects.filter(school=school_object).values('name'))
-    list_of_years = []
-    for school in available_year:
-        list_of_years.append(school['name'])
-    return list_of_years
     
-
 #Serializer for creating courses
 class CreateCourseSerializer(serializers.ModelSerializer):
-    instructor_name = serializers.ChoiceField(choices=[])
-    year_name = serializers.ChoiceField(choices=[])
-
     class Meta:
         model = Courses
-        fields = ['title', 'year_name', 'instructor_name', 'credit']
+        fields = ['title', 'year', 'instructor_name', 'credit']
         extra_kwargs = {
             'title': {'required': True},
             'credit': {'required': True},
         }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.set_dynamic_choices()
-
-    def set_dynamic_choices(self):
-        user = self.context["request"].user
-        self.fields["year_name"].choices = get_available_year(user)
-        available_instructors = get_available_instructors(user)
-        self.fields["instructor_name"].choices = [(name, name) for name in available_instructors]
 
     def save(self, **kwargs):
         school_object = Administrator.objects.get(user=self.context["request"].user)
-        year_name = self.validated_data['year_name']
+        year= self.validated_data['year']
         instructor_name = self.validated_data['instructor_name']
-        year_object = Year.objects.get(name=year_name, school=school_object)
+        year_object = Year.objects.get(name=year, school=school_object)
         instructor_object = Instructor.objects.get(name=instructor_name, school=school_object)
         Courses.objects.create(
             title=self.validated_data['title'],
